e4131ae446f890debbc85a33c16d59024e8df07f
QuangHuy79 <toquanghuytktt.com>
2025-07-26 08:08:21 +0700

    Them chi tiet san pham

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,28 @@
+// import "./App.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+// import ResponsiveProductsGrid from "./Components/Products/ResponsiveProductsGrid";
+// import ReactHeaderTemplates from "./Components/Footer_Header/ReactHeaderTemplates";
+// import ReactFooterTemplates from "./Components/Footer_Header/ReactFooterTemplates";
+// import ShoppingCartTemplate from "./Components/shoppingCart/ShoppingCartTemplate";
+// import { CartProvider } from "./Components/context/CartContext";
+
+// function App() {
+//   return (
+//     <CartProvider>
+//       <BrowserRouter>
+//         <ReactHeaderTemplates />
+//         <ResponsiveProductsGrid />
+//         <ReactFooterTemplates />
+//         <ShoppingCartTemplate />
+//       </BrowserRouter>
+//     </CartProvider>
+//   );
+// }
+
+// export default App;
+
+// ****** Thêm ProductDetail *******
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
@@ -6,13 +31,18 @@ import ReactHeaderTemplates from "./Components/Footer_Header/ReactHeaderTemplate
 import ReactFooterTemplates from "./Components/Footer_Header/ReactFooterTemplates";
 import ShoppingCartTemplate from "./Components/shoppingCart/ShoppingCartTemplate";
 import { CartProvider } from "./Components/context/CartContext";
+import ProductDetail from "./Components/ProductDetail/ProductDetail"; // Thêm dòng này
 
 function App() {
   return (
     <CartProvider>
       <BrowserRouter>
         <ReactHeaderTemplates />
-        <ResponsiveProductsGrid />
+        <Routes>
+          <Route path="/" element={<ResponsiveProductsGrid />} />
+          <Route path="/product/:id" element={<ProductDetail />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
         <ReactFooterTemplates />
         <ShoppingCartTemplate />
       </BrowserRouter>
